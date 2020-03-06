fff4e411e48f19075331a0e3afcddfb937a4aad1
Zeynep Yucel <33271745+yucelzeynep@users.noreply.github.com>
2020-03-06 10:42:44 +0900

    Add files via upload

---
+++ codes/myobject.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 10:22:57 2020
+
+@author: zeynep
+"""
+import numpy as np
+import pickle
+
+import sys
+sys.path.insert(0, '../') # for constants, and preferences
+
+import cv2
+from scipy import ndimage
+
+import fig_tools as fig_tools
+
+from importlib import reload
+import preferences
+reload(preferences)
+
+import constants
+reload(constants)
+
+
+class MyObject():
+    """
+    MyObject is a collection of data regarding a single object (or equivalently an image).
+    It does not depend on participant, motivation etc.
+    """
+    def __init__(self):
+        self.image_fname = [] # jpeg name
+        
+        self.image_orig = []
+        self.image_fg_binary = [] # foreground (after thresholding)
+        
+        self.object_type = [] # one of the six object types
+        
+        self.object_long_side = 0
+        self.object_short_side = 0
+        self.object_size = 0
+        
+        self.smap = {} # saliency map
+        for saliency_type in preferences.SALIENCIES:
+            self.smap[saliency_type] = []
+        
+        self.polygon_functional = [] # handle, grip
+        self.polygon_manipulative = [] # end effector
+        
+    def builder(self, image_path):
+        """
+        Fill in the variables
+        """
+        
+        self.image_fname = image_path.split('/')[-1]
+        self.object_type = image_path.split('/')[-2]
+        self.load_image(image_path)
+        self.get_fg_binary()                   
+        self.set_object_dims()        
+        self.build_saliency_map()        
+        self.load_polygons()
+        
+#        # save myobject object
+#        fpath = constants.OUTPUT_DIR + 'images/' + self.image_fname.split('.')[0] + '.pkl'
+#        with open(str(fpath), 'wb') as f:
+#            pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
+        
+    def load_image(self, image_path):
+        """
+        Load the original image
+        """
+        # second variable is set to 0 for reading at gray level, to 1 for color
+        self.image_orig = cv2.imread(image_path, 0)
+            
+  			
+    
+    def compute_smap_specres(self):
+        """
+        Compute the saliency map with spectral residual
+        """
+        saliency_static_specres = cv2.saliency.StaticSaliencySpectralResidual_create()
+
+        if preferences.SALIENCIES['STATIC_SPECRES']:
+            (success, saliencyMap_static_specres) = saliency_static_specres.computeSaliency(\
+            self.image_orig)
+            saliencyMap_static_specres = (saliencyMap_static_specres * 255).astype("uint8")
+            
+            self.smap['STATIC_SPECRES'] = saliencyMap_static_specres
+      
+            fig_tools.scale_and_display(saliencyMap_static_specres, "STATIC_SPECRES")
+            
+    def compute_smap_finegrain(self):
+        """
+        Compute the saliency map with fine grain
+        """
+        saliency_static_finegrain = cv2.saliency.StaticSaliencyFineGrained_create()
+        
+        if preferences.SALIENCIES['FINE_GRAIN']:
+            (success, saliencyMap_static_finegrain) = saliency_static_finegrain.computeSaliency(self.image_orig)
+            saliencyMap_static_finegrain = (saliencyMap_static_finegrain * 255).astype("uint8")
+            
+            self.smap['FINE_GRAIN'] = saliencyMap_static_finegrain                       
+            
+            fig_tools.scale_and_display(saliencyMap_static_finegrain, "FINE_GRAIN")
+            
+    def compute_smap_objectness(self):
+        """
+        Compute the saliency map with objectness
+        """
+        # For this data set, max object detections per image is 1
+        MAX_DETECTIONS = 1 
+        
+        output = []
+        saliency_objnes = cv2.saliency.ObjectnessBING_create()
+        saliency_objnes.setTrainingPath('objectness_trained_model/')
+            
+        if preferences.SALIENCIES['OBJECTNESS']:                    
+            (success, saliencyMap_objness) = saliency_objnes.computeSaliency( self.image_orig )
+            numDetections = saliencyMap_objness.shape[0]
+        
+            # loop over the detections (redundant)
+            for i in range(0, min(numDetections, MAX_DETECTIONS)):
+                	# extract the bounding box coordinates
+                	(startX, startY, endX, endY) = saliencyMap_objness[i].flatten()
+                	
+                	# randomly generate a color for the object and draw it on the image
+                	output = self.image_orig.copy()
+                	color = np.random.randint(0, 255, size=(3,))
+                	color = [int(c) for c in color]
+                	cv2.rectangle(output, (startX, startY), (endX, endY), color, 2)
+                
+            self.smap['OBJECTNESS'] = saliency_objnes
+                
+            fig_tools.scale_and_display(output, 'OBJECTNESS')
+            
+    def build_saliency_map(self):
+        """
+        Compute salinecy maps accordong to different paradigms
+        Check preferences for the leys to control types of saliency
+        """
+        self.compute_smap_specres()
+        self.compute_smap_finegrain()
+        self.compute_smap_objectness()
+            
+    def get_fg_binary(self):
+        """
+        the image dispayed to the participant is gray scale. but there are 3 
+        channels
+        """
+        
+        fg_image = self.image_orig.copy()
+        
+        """
+        No need for conversion to gray scale, if you read the image already as 
+        gray scale as follows
+        cv2.imread(xxx, 0)
+         
+        Otherwise, threshold using
+        fg_gray = cv2.cvtColor(fg_image, cv2.COLOR_BGR2GRAY)
+        """
+
+        # thresholding
+        ret3,fg_binary = cv2.threshold(fg_image, 0, 255, \
+                                       cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV) # cv2.THRESH_BINARY+
+
+        # morphological operations
+        kernel = np.ones((2,2),np.uint8)
+        fg_binary = cv2.morphologyEx(fg_binary, cv2.MORPH_CLOSE, kernel)
+        
+        self.image_fg_binary = fg_binary.copy()
+
+       
+        
+    def set_object_dims(self):
+        """
+        Set size (number of white pixels on fg image) and 
+        lond and short sides of the object
+        """
+        self.object_size = int(np.sum(self.image_fg_binary)/255)
+            
+    def load_polygons(self):
+        """
+        Load annotated polygons for functional and manipulative object parts
+        """
+        # functional
+        poly_func_fname = constants.ANNOTATION_POLYGON_FUNCTIONAL_DIR + self.image_fname.replace('.jpeg', '_halfsize.txt')
+        
+        # polygons are annotated on half-size images so scale back
+        poly_func = np.multiply( \
+                                np.loadtxt(poly_func_fname, \
+                                           dtype='i', delimiter=' '), 2)
+        
+        self.polygon_functional = poly_func
+                
+        # manipulative
+        poly_manip_fname = constants.ANNOTATION_POLYGON_MANIPULATIVE_DIR + self.image_fname.replace('.jpeg', '_halfsize.txt')
+        poly_manip =  np.multiply( \
+                                  np.loadtxt(\
+                                             poly_manip_fname, \
+                                             dtype='i', delimiter=' '), 2)
+        
+        self.polygon_manipulative = poly_manip
+            
+               
+            
+            
+            
+        
+        
+        
+        
+        
---
+++ codes/person.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 14:47:38 2020
+
+For fixation density map, refer to 
+
+https://github.com/takyamamoto/Fixation-Densitymap
+
+
+@author: zeynep
+"""
+
+import numpy as np
+import pickle
+import copy
+
+import cv2
+
+import sys
+sys.path.insert(0, '../')# for file_tools etc
+sys.path.insert(0, '../arrange_exp_data')# for person class
+
+import matplotlib.pyplot as plt
+import file_tools as file_tools
+import saliency_tools as saliency_tools
+import fig_tools as fig_tools
+
+
+from myobject import MyObject
+
+from importlib import reload
+
+import constants
+reload(constants)
+
+import preferences
+reload(preferences)
+
+
+class Person():
+    """
+    Person is a collection of data of a single participant
+    It involves all image names and gaze data
+    """
+    def __init__(self):
+        self.name = []
+        self.age = []  
+        self.age_range = []
+        self.motiv = []        
+        
+        self.image = {}
+        
+        for object_type in constants.OBJECT_TYPES:
+            """
+            Here, I need to keep several structures as below, since some images 
+            (target and blank) are displayed multile times, so I cannot use 
+            simply their names as dictionary keys
+            
+            For initializing fixation map, I use screen_height and screen_width.
+            Actualy these are the same as image_height and image_width,
+            since I display the images in full screen.
+            But this is not necessarily true for any experiment.
+            To highlight that, here I use screen params
+            """
+            self.image[object_type] = {'image_fnames':[],\
+                                      'image_fixations': [],
+                                      'fmaps': [],\
+                                      'familiarities': []}
+            
+        
+    def builder(self, participant):
+        """
+        Fill in the variables
+        """
+        self.get_name(participant)
+        self.load_age(participant)
+        self.get_motiv(participant)
+        self.load_image_related(participant)
+        self.build_fmap()
+        self.load_familiarities(participant)
+                
+#        # save person object
+#        fpath = constants.OUTPUT_DIR + 'person/' + participant + '.pkl'
+#        with open(str(fpath), 'wb') as f:
+#            pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
+
+       
+        
+    def get_name(self, participant):
+        """
+        This function picks up the last name of the participant
+        
+        This information is at 5th place in the parsed string
+        """
+        self.name =  participant.split("_")[5]
+        
+    def load_age(self, participant):
+        """
+        This functions load the age and sets the age_range.
+        
+        Anyone younger than 30 has an age range of young,
+        and others have an age_range of old.
+        
+        the file with ages is not provided in the repository due to privacy 
+        concerns.
+        """
+        age_list = np.genfromtxt(constants.AGE_FILE_PATH, delimiter='\t', dtype='U')
+        for age_line in age_list:
+            if age_line[0] == participant:
+                self.age = int(age_line[1] )
+                if self.age < 30:
+                    self.age_range = 'young'
+                else:
+                    self.age_range = 'old'
+        
+    def get_motiv(self, participant):
+        """
+        This information is at the 0th letter of at 4th place in the parsed string
+        
+        Motivation possibilities together with their abbreviations are:
+            f: freeview
+            u: use
+            p: push
+            
+        """
+        m = participant.split("_")[4][0] # 4th string 0th letter 
+        self.motiv = m
+        
+    def load_image_related(self,participant):
+        """
+        This field does not contain the image itself (i.e. jpeg file) but gaze 
+        information of the participant over the images.
+        
+        The image variable has the following shape:
+            image[object_type][image_fnames]
+            image[object_type][image_fixations]
+            image[object_type][fmaps]
+            image[object_type][familiarities]
+            
+        I need to keep several structures as above, since some images 
+        (target and blank) are displayed multiple times, so I cannot use simply
+        their names as dictionary keys.
+            
+        Into fixations, I copy the gaze as it is logged i.e.:
+            px py t
+        px and py are x- and y-coordinates, and t is timestamp in sec (with
+        3 digits after decimal point, so millisec information is also there)
+        """
+    
+        gaze_path = constants.RAW_GAZE_PATH + participant + '/' 
+        gaze_fname = file_tools.find_file_in_path('*_gaze.txt', gaze_path)[0]
+        gaze_totdur = np.loadtxt(gaze_fname) # gaze over all viewing sessions
+        
+        """
+        Timelog has the following structure:
+            image_fname t0 tf
+        
+        Here, t0 is the instant when the image is displayed and tf is the 
+        instant when it is removed. 
+        """
+        timelog_fnames = np.sort(file_tools.find_file_in_path('*_timelog.txt', gaze_path))            
+                
+        for t, timelog_fname in enumerate(timelog_fnames):
+            timelog = np.genfromtxt(timelog_fname,\
+                             dtype=None, delimiter='\t',encoding='ASCII', \
+                             names=('image_fname', 't0', 'tf'))
+            
+            object_type = constants.OBJECT_TYPE_VS_TIMELOG[str(t)]
+            
+            for timewindow in timelog:
+                image_fname = timewindow[0]
+                t0 = timewindow[1]
+                tf = timewindow[2]
+                
+                self.image[object_type]['image_fnames'].append(image_fname)
+                 
+                temp_fixations = [] # gaze over a single image
+                # append to array one by one
+                for fixation in gaze_totdur:
+                    fixation_time = fixation[2] # this is in sec (ie ss.mmm)
+                    if fixation_time*1000 >t0 and fixation_time*1000 <= tf:
+                        temp_fixations.append(fixation)
+                        
+                self.image[object_type]['image_fixations'].append(temp_fixations)
+                    
+              
+    
+    
+
+    
+    def build_fmap(self):
+        """
+        Build fixation maps. 
+        """
+          
+        for object_type in preferences.OBJECT_TYPES_INTEREST:
+            for image_fname, image_fixation in zip(\
+                                                   self.image[object_type]['image_fnames'], \
+                                                   self.image[object_type]['image_fixations'] ):
+                                
+                if image_fname != constants.TARGET_IMAGE_FNAME and\
+                image_fname != constants.BLANK_IMAGE_FNAME:
+                    
+                    
+                    # from discrete gaze samples to a distribution
+                    fmap = saliency_tools.Fixpos2Densemap(image_fixation, \
+                                                          constants.IMAGE_WIDTH, \
+                                                          constants.IMAGE_HEIGHT)
+                      
+                    """
+                    This part is for displaying image_orig, fixations and fmap 
+                    Just for keeping track of what is happening
+                    """
+                    # load myobject and image_orig
+                    myobject_fpath = constants.INPUT_DIR + 'images/' + image_fname.replace('jpeg', 'pkl')
+                    with open(myobject_fpath,'rb') as f:
+                        myobject = pickle.load(f)
+                        
+                    # image with fixations over image_orig
+                    image_temp = cv2.merge([myobject.image_orig, myobject.image_orig, myobject.image_orig])
+                    for g in image_fixation:
+                        cv2.circle(image_temp, (int(g[0]), int(g[1])), 5, (10,255,25), -1)
+                        
+                    fig_tools.scale_and_display(myobject.image_orig, 'image_orig')
+                    fig_tools.scale_and_display(image_temp, 'image_with_fixations')
+                    fig_tools.scale_and_display(fmap, 'fmap')               
+                            
+                else:
+                    # for target and blank image, I donot care about fmaps
+                    fmap = []
+                    
+               
+                self.image[object_type]['fmaps'].append(fmap)
+
+                
+
+                
+                
+    def load_familiarities(self, participant):
+        """
+        Load familiarities from the txt files.
+        
+        A familiarity of -1 indicates that no value is entered. In this case:
+            1. The image is blank or target.
+            2. The particpant did not see that page and did not write any scores 
+            (There is one such person. Her name is not released due to privacy concerns)
+
+        Note that I did not collect familiarity score for the objects in the
+        test run, because it is not relevant for the experiment. 
+        Therefore, it catches the exception for these images.
+        """
+        fname = constants.FAMILIARITY_PATH + participant + '_familiarity.txt'
+        scores = np.genfromtxt(fname, delimiter='\t', dtype='U')
+        temp_names = [ l[0] for l in scores ]
+        temp_scores = [ l[1] for l in scores ]
+        
+        
+        for object_type in constants.OBJECT_TYPES:
+            temp = []
+            for image_fname in self.image[object_type]['image_fnames']:
+                if image_fname != constants.BLANK_IMAGE_FNAME and\
+                image_fname != constants.TARGET_IMAGE_FNAME:
+                    
+                    try:
+                        ind = temp_names.index(image_fname)
+                        temp.append(int (temp_scores[ind]) )
+                    except ValueError:
+                        print('Image fname is not found {}'.format((image_fname)))
+                        temp.append( -1 )
+                else:
+                    temp.append( -1 )
+                    
+                    
+            self.image[object_type]['familiarities'] = \
+            copy.deepcopy( temp )
+        
+            print('{} {}'.format(object_type, self.image[object_type]['familiarities']))
+        
+             
